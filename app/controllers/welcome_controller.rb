9b640f8d9a5df977e818e3ed6f8646bb46d2803a
CamilaMarin <camila.marin@usach.cl>
2014-12-07 17:43:35 -0300

    busca tweets asociados a palabras_claves y hashtags - busca las palabras de la bolsa en el texto

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,17 +8,30 @@ class WelcomeController < ApplicationController
     end   
     #tabla de Palabras Claves
     @tabla = PalabraClave.all()
+    #tabla de Hashtags
+    @tabla2 = Hashtag.all()
+    #Seleccion de la columna de los nombres de los hashtags
+    @hashtags = @tabla2.select("nombre_hashtag")
     #Seleccion de la columna de los nombres de las palabras claves
-    @topics = @tabla.select("nombre_palabra_clave")
+    @palabras_clave = @tabla.select("nombre_palabra_clave")
+    cont = 0
+    @topics = []
+    @hashtags.each do |h|
+      @topics[cont] = h.nombre_hashtag
+      cont += 1
+    end
+    @palabras_clave.each do |p|
+      @topics[cont] = p.nombre_palabra_clave
+      cont += 1 
+    end
     #tabla de la bolsa de palabras
-    @palabras_bolsa = PalabraBolsa.all()
-    #Seleccion de la columna de los nombres de la bolsa de palabras
-    @palabras = @palabras_bolsa.select("nombre_palabra")
+    @tabla1 = PalabraBolsa.all()
     #contador correspondiente a los id
-  	contador = 0000
+  	contador = 0
+    contador1 = 0
    	@topics.each do |t|
       #Busca tweet asociados a las palabras claves
-    	@search = $client.search(t.nombre_palabra_clave, :include_rts => false, :lang => "es").take(1).collect  
+    	@search = $client.search(t, :include_rts => false, :lang => "es").take(1).collect
   	 	@search.each do |tweets| 
   		   @texto = Texto.new({id_tweet: tweets.id, id_texto: contador,texto: tweets.text}); 
   		   if Texto.where(id_tweet: @texto.id_tweet).count > 0 #si ya existe el tweet no lo agrega
@@ -30,14 +43,30 @@ class WelcomeController < ApplicationController
             #crea y guarda el id del tweet, id del texto, id del usuario, la region(si la tiene) y la fecha de cración del tweet
   			   	@tweet = Tweet.create({id_tweet: tweets.id, id_texto: contador,id_usuario: tweets.user.id, region: tweets.user.location, fecha: tweets.created_at})
             #busca el id del actor asociado al tweet
-            @id__actor = @tabla.select("id_actor").where(nombre_palabra_clave: t.nombre_palabra_clave).limit(1)
+            @palabras_clave.each do |pl|
+              if t == pl.nombre_palabra_clave
+                @id__actor = @tabla.select("id_actor").where(nombre_palabra_clave: t).limit(1)
+              end
+            end
+            @hashtags.each do |ht|
+              if t == ht.nombre_hashtag
+                @id__actor = @tabla2.select("id_actor").where(nombre_hashtag: t).limit(1) 
+              end
+            end
             @id__actor.each do |a|
               id_actor_a = a.id_actor
               #crea y guarda el id del actor-tweet, el id del actor y el id del tweet
               @actor = ActorTweet.create({id_at: contador, id_actor: id_actor_a, id_tweet: tweets.id})
             end
 
-
+            str=tweets.text
+            arreglo_texto=str.split(" ")
+            @tabla1.each do |t1|
+              if arreglo_texto.index t1.nombre_palabra
+                @palabra_bolsa=TextoPalabra.create({id_tp: contador1, id_texto: contador, id_palabrab: t1.id_palabrab})
+                contador1+=1
+              end
+            end
   		   		contador += 1
   		    end
   		end
